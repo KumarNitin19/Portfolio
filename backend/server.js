b19387269c9f031f08c6fc27e78081292d5a6d75
Nitin <88976150+Nitinkumar192001@users.noreply.github.com>
2022-05-30 01:13:00 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const dotenv = require("dotenv");
 const connectDB = require('./Config/db');
 const cors = require('cors');
+const path = require('path');
 
 const InfoRoute = require('./Router/route')
 
@@ -28,19 +29,18 @@ app.use(bodyParser.json());
 
 
 
-
+const __dirname1 = path.resolve();
 if(process.env.NODE_ENV === 'production'){
 
-    app.use(express.static('portfolio/dist/portfolio'));
-
-    app.get('*', (req, res) =>
-        res.sendFile('index.html', {root: 'portfolio/dist/portfolio/'}),
-    );
+    app.use(express.static(path.join(__dirname1,"portfolio/dist/portfolio")))
+ 
+    app.get('*',(req,res)=>{
+     res.sendFile(path.resolve(__dirname1,"portfolio/dist/portfolio/index.html"))
+   })
   
 }else{
     app.use(express.static('portfolio/src'));
-
-    app.get('*', (req, res) =>
+    app.get('/', (req, res) =>
         res.sendFile('index.html', {root: 'portfolio/src/'}),
     );
 }
